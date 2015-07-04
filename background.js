7128295c92e0bbbb57f0735fc6bdd092bf8962f9
Dean Lee <xslidian@gmail.com>
2015-07-04 14:15:15 +0800

    clean-up from html to js

--- background.js
+++ background.js
@@ -1,29 +1,9 @@
-<!doctype html>
-<meta content=text/html;charset=utf-8 http-equiv=Content-Type />
-<title i18n-content=extension_name>VeryCD Notifications</title>
-<script>
 	var VeryCD={Members:{Messages:{'unRead':-1,'sinat':false,'qqt':false}}};
 	chrome.browserAction.onClicked.addListener(function() {
 		chrome.tabs.create({url: 'http://home.verycd.com/space.php?do=pm&filter=newpm'})});
 	chrome.browserAction.setBadgeText({text:'...'});
 	chrome.browserAction.setIcon({path:'icons/icon.png'});
-</script>
-<script id="datajs" src="http://www.verycd.com/ajax/member?m=messages&format=js"></script>
-<script>
-	if (!Array.prototype.forEach) {
-		Array.prototype.forEach = function(fun) {
-			var len = this.length;
-			if (typeof fun != 'function')
-				throw new TypeError();
 
-			var thisp = arguments[1];
-			for (var i = 0; i < len; i++)
-			{
-				if (i in this)
-					fun.call(thisp, this[i], i, this);
-			}
-		};
-	}
 	function attachnum(element, index, array) {
 		if (element.selected==true)
 			chrome.browserAction.setBadgeText({text:VeryCD.Members.Messages.unRead.toString(),tabId:element.id});
@@ -53,5 +33,3 @@
 	setInterval("updatedata();", 10000);
 	setInterval("updatebadge();", 1000);
 	//chrome.windows.onFocusChanged.addListener(function(integer windowId) {updatebadge()});
-	</script>
-<!--body onload="updatebadge();"-->
